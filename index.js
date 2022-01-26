8ffc9838b115fad5d7afc8d30dfcf66a7751cec9
boogameow <76601381+boogameow@users.noreply.github.com>
2022-01-25 19:57:35 -0800

    reverted

--- index.js
+++ index.js
@@ -24,15 +24,11 @@ function template(id){
 io.on('connection', (socket) => {
     var user = new template(socket.id);
     user.id = socket.id;
-    user.socket = socket
     users[socket.id] = user;
 
     // disconnect hookup
     socket.on("disconnect", ()=>{
-        if (users[users[socket.id].id2] !== null){
-            users[socket.id].socket2.emit("stop")
-        }
-        
+        io.emit("stop", users[socket.id])
         delete users[socket.id];
         io.emit('usercount', Object.keys(users).length)
     })
@@ -41,30 +37,25 @@ io.on('connection', (socket) => {
     socket.on("paddle", (data)=> {
         users[socket.id].y = data.contents[0]
         users[socket.id].dir = data.contents[1]
-
-        if (users[users[socket.id].id2] !== null){
-            users[socket.id].socket2.emit("paddle", users[socket.id])
-        }
+        io.emit("paddle", users[socket.id])
     })
 
     // ball movement
     socket.on("ball", (data)=> {
-        if (users[socket.id].host == true && users[users[socket.id].id2] !== null){
-            users[socket.id].socket2.emit("ball", {
-                x: data.contents[0],
-                y: data.contents[1]
-            })
-        }
+        io.emit("ball", {
+            x: data.contents[0],
+            y: data.contents[1],
+            id: users[socket.id].id
+        })
     })
 
     // dub scores
     socket.on("score", (data)=> {
-        if (users[socket.id].host == true && users[users[socket.id].id2] !== null){
-            users[socket.id].socket2.emit("score", {
-                blue: data.contents[0],
-                red: data.contents[1]
-            })
-        }
+        io.emit("score", {
+            blue: data.contents[0],
+            red: data.contents[1],
+            id: users[socket.id].id
+        })
     })
 
     // send the client starting data.
@@ -75,15 +66,12 @@ io.on('connection', (socket) => {
     Object.keys(users).forEach(u => {
         if (users[u].id !== socket.id && users[u].found == null && users[socket.id].found == null){
             socket.emit('join', {user: users[u], id: u, host: false});
-            users[u].socket.emit('join', {user: users[u], id: u, host: true});
+            io.emit('join', {user: users[u], id: u, host: true});
             users[socket.id].found = true
+            users[socket.id].id = u
             users[socket.id].host = false
-            users[socket.id].socket2 = users[u].socket
-            users[socket.id].id2 = u
             users[u].found = true
             users[u].host = true
-            users[u].id2 = socket.id
-            users[u].socket2 = users[socket.id].socket
         }
     });
 })
